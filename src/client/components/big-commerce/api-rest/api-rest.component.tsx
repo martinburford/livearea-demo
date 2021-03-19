da84fae2cf5ce2c83a7ce0fa070aea7420da845f
Martin Burford <mburford@liveareacx.com>
2021-03-19 15:15:47 +0000

    Public proxy added to allow API access

--- src/client/components/big-commerce/api-rest/api-rest.component.tsx
+++ src/client/components/big-commerce/api-rest/api-rest.component.tsx
@@ -2,14 +2,14 @@ import Axios from "axios";
 import React from "react";
 
 export const ApiRest: React.FC = () => {
-  Axios.get("https://api.bigcommerce.com/stores/gpiw17bnmw/v3/catalog/products", {
+  Axios.get("https://cors.bridged.cc/https://api.bigcommerce.com/stores/gpiw17bnmw/v3/catalog/products", {
     headers: { "X-Auth-Token": "a2iuhtb71dld85wjrzdxi023x3s8y2h" }
   }).then((response) => {
     console.log("Response", response);
   }).catch((error) => {
     console.log("Error", error);
   });
-  
+
   return <p>API Rest</p>;
 };
 
